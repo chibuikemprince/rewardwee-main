f069452348bf66ee34031fe93b1e7c868fbeaad8
chibuikemprince <youngprince042@yahoo.com>
2023-07-19 13:56:12 +0100

    serverless deploy v2

--- src/modules/types.ts
+++ src/modules/types.ts
@@ -1,8 +1,14 @@
 
-import { SubscriptionPlan, Currency  } from "../../../rewardwee_database/src/types/index"
+import { SubscriptionPlan, CurrencyArray as CA} from "reward_service_store_schemas"
+ 
 
-export type CurrencyType =  keyof typeof Currency;
-export const CurrencyArray: Currency[]  =  Object.values(Currency);
+export type CurrencyType =   typeof CurrencyArray[number];
+
+export const CurrencyArray  =  CA;
+
+
+
+console.log({CurrencyArray})
 
 export interface SubscriptionPlanType extends SubscriptionPlan {}
 
