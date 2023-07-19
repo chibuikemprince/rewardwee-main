a69f3264f4a82ec18c9d212aeb4057007aeef026
chibuikemprince <youngprince042@yahoo.com>
2023-07-19 11:31:04 +0100

    subscription db collection and schema connectd from rewardwee custom npm store module

--- src/databases/external.ts
+++ src/databases/external.ts
@@ -5,17 +5,17 @@ import {
 
  
 
-import {   LoginRecord, UserLoginRecordModelData, UserLoginRecordSchema  }  from "reward_service_store_schemas"
+import {  
+  
+   LoginRecord, UserLoginRecordModelData, UserLoginRecordSchema,
+  SubscriptionPlanDataModel, SubscriptionPlanSchema,SubscriptionPlan
+
+}  from "reward_service_store_schemas"
  
 
 //import {LoginRecord, UserLoginRecordData}  from "../../../rewardwee_database/src/login"
 export const UserLoginRecord =  model<LoginRecord>(UserLoginRecordModelData.name, UserLoginRecordSchema);
-
-/*  
-  */
-
-import { SubscriptionPlanDataModel, SubscriptionPlanSchema } from "../../../rewardwee_database/src/plans/plans"
-import { SubscriptionPlan  } from "../../../rewardwee_database/src/types/index"
+ 
 
 
 export const AllSubscriptionPlans =  model<SubscriptionPlan>(SubscriptionPlanDataModel.name, SubscriptionPlanSchema);
